299b41f49680e4cb8da87c16a3d8d63913f42169
aKKKo <AkinKonstantin@yandex.ru>
2022-07-31 20:30:39 +0300

    create layouts

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="navbar">
+    <h3>Online Bank</h3>
+
+    <ul class="navbar-menu">
+      <li>
+        <router-link to="/">Заявки</router-link>
+      </li>
+      <li>
+        <router-link to="/">Помощь</router-link>
+      </li>
+      <li>
+        <router-link to="/">Сообщения</router-link>
+      </li>
+      <li>
+        <router-link to="/">Выход</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/layout/AuthLayout.vue
@@ -0,0 +1,20 @@
+<template>
+
+  <div class="container">
+    <div class="card">
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+  export default {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
