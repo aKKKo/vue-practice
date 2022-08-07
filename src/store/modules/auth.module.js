7a3752654d237069a423c742ea70af124f17c24a
aKKKo <AkinKonstantin@yandex.ru>
2022-08-07 18:16:57 +0300

    Implemented authorization through the server Firebase. Created a visual error handler with new components and message output to the user's screen.

--- src/store/modules/auth.module.js
+++ src/store/modules/auth.module.js
@@ -1,3 +1,6 @@
+import axios from "axios"
+import {error} from '../../utils/error'
+
 const TOKEN_KEY = 'jwt-token'
 
 export default {
@@ -18,8 +21,19 @@ export default {
     }
   },
   actions: {
-    async login({commit}, payload) {
-      commit('setToken', 'TEST TOKEN')
+    async login({commit, dispatch}, payload) {
+      try {
+        const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.VUE_APP_FB_KEY}`
+        const {data} = await axios.post(url, {...payload, returnSecureToken: true})
+        commit('setToken', data.idToken)
+        commit('clearMessage', null, {root: true})
+      } catch(e) {
+        dispatch('setMessage', {
+          value: error(e.response.data.error.message),
+          type: 'danger'
+        }, {root: true})
+        throw new Error()
+      }
     }
   },
   getters: {
