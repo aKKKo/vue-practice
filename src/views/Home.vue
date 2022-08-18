5f2a3b36c7d250f3df7dec53762fd8140a3ffae8
aKKKo <AkinKonstantin@yandex.ru>
2022-08-18 20:53:48 +0300

    Creating a request navigation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <app-loader v-if="loading" />
-  <app-page title="Спиок заявок" v-else>
+  <app-page title="Список заявок" v-else>
     <template #header>
       <button class="btn primary" @click="modal = true">Создать</button>
     </template>
-
+    <request-filter v-model="filter"></request-filter>
     <request-table :requests="requests"></request-table>
 
     <teleport to="body">
@@ -20,6 +20,7 @@ import { ref, computed, onMounted } from 'vue'
 import AppPage from '../components/ui/AppPage'
 import RequestTable from '../components/request/RequestTable'
 import RequestModal from '../components/request/RequestModal'
+import RequestFilter from '@/components/request/RequestFilter'
 import AppModal from '../components/ui/AppModal'
 import AppLoader from '../components/ui/AppLoader'
 import { useStore } from 'vuex'
@@ -29,20 +30,35 @@ export default {
     const store = useStore()
     const modal = ref(false)
     const loading = ref(false)
+    const filter = ref({})
 
     onMounted(async () => {
       loading.value = true
       await store.dispatch('request/load')
       loading.value = false
     })
-    const requests = computed(() => store.getters['request/requests'])
+    const requests = computed(() => store.getters['request/requests']
+      .filter(request => {
+        if(filter.value.name) {
+          return request.fio.includes(filter.value.name)
+        }
+        return request
+      })
+      .filter(request => {
+        if (filter.value.status) {
+          return filter.value.status === request.status
+        }
+        return request
+      })
+    )
 
     return {
       modal,
       requests,
-      loading
+      loading,
+      filter
     }
   },
-  components: { AppPage, RequestTable, AppModal, RequestModal, AppLoader }
+  components: { AppPage, RequestTable, AppModal, RequestModal, AppLoader, RequestFilter }
 }
 </script>
