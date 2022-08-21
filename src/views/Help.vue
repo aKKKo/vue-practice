4a6690e018bef997d5d1f789b11c1d1d84c1d004
aKKKo <AkinKonstantin@yandex.ru>
2022-08-21 14:21:32 +0300

    Creating the Help page

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <app-page back title="Помощь">
+    <question-item
+      v-for="q in questions"
+      :key="q.title"
+      :question="q"
+    ></question-item>
+  </app-page>
 </template>
+
+<script>
+import AppPage from '@/components/ui/AppPage.vue'
+import QuestionItem from '@/components/question/QuestionItem.vue'
+
+
+export default {
+  setup() {
+    const questions = [
+      {title: 'Вопрос 1', text: 'Ответ 1'},
+      {title: 'Вопрос 2', text: 'Ответ 2'}
+    ]
+
+    return {
+      questions
+    }
+  },
+  components: {
+    AppPage,
+    QuestionItem
+  }
+}
+</script>
